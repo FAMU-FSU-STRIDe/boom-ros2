a79eb446ab1aaa02399c42c1283ab5ae8ce87b61
jtylerboylan <jtylerboylan@outlook.com>
2023-07-11 09:21:52 -0400

    v1.2

--- starq_motor_controller/starq_motor_controller/starq_motor_controller_node.py
+++ starq_motor_controller/starq_motor_controller/starq_motor_controller_node.py
@@ -1,10 +1,9 @@
+from can import Notifier
 import rclpy
 from rclpy.node import Node
-
 from starq_interfaces.msg import *
 from starq_interfaces.srv._configure_motors import *
-
-import starq_motor_controller.odrive_can_functions as canfunc
+from starq_motor_controller.odrive_can_tool import ODriveCANTool
 
 class STARQMotorDriverNode(Node):
 
@@ -12,7 +11,12 @@ class STARQMotorDriverNode(Node):
         super().__init__('motor_driver')
         self.get_logger().info("Starting motor controller node.")
 
-        canfunc.set_logger(self.get_logger())
+         # From odrive_can_tool.py
+        self.cantool = ODriveCANTool(self)
+
+        if self.cantool.canok: # Check if CAN successfully initialized
+            # Create CAN notifier
+            self.cannotify = Notifier(self.cantool.canbus, [self.cantool])
 
         self.motor_confs : list[ODriveConfig] = []
         self.last_cmds : list[ODriveCommand] = []
@@ -28,89 +32,53 @@ class STARQMotorDriverNode(Node):
 
     # Send motor commands
     def cmd_motors_callback(self, msg : ODriveCommandArray):
-        if len(self.motor_confs) == 0:
+        if len(self.motor_confs) == 0: # Check for empty config list
             self.get_logger().warn("No motors configured!")
         for config in self.motor_confs:
             try:
+                # Check if command size and config size match
                 cmd : ODriveCommand = msg.commands[config.id]
             except IndexError:
                 self.get_logger().error(f"Missing motor command for id: {config.id}")
                 continue
-            if config.control_mode == 3:
-                canfunc.set_position(config.can_id, 
-                                     position=cmd.input_position * config.gear_ratio, 
-                                     velocity_ff=cmd.input_velocity * config.gear_ratio, 
-                                     torque_ff=cmd.input_torque / config.gear_ratio)
-            elif config.control_mode == 2:
-                canfunc.set_velocity(config.can_id,
-                                     velocity=cmd.input_velocity * config.gear_ratio,
-                                     torque_ff=cmd.input_torque / config.gear_ratio)
-            elif config.control_mode == 1:
-                canfunc.set_torque(config.can_id,
-                                   torque=cmd.input_torque / config.gear_ratio)
-        self.last_cmds = msg.commands
+            self.cantool.send_command(config.can_id, cmd, config.control_mode)
+        self.last_cmds = msg.commands # Update latest commands
 
     # Configure motors
     def conf_motors_callback(self, request : ConfigureMotors_Request, response):
-        self.max_motor_id = -1
+        self.max_motor_id = -1 # Keep track of largest motor id
         self.motor_confs = list[ODriveConfig](request.configs)
         for config in self.motor_confs:
-            can_id = config.can_id
-            if config.requested_state == 0:
-                canfunc.emergency_stop(can_id) # Emergency stop
-                continue
-            canfunc.clear_errors(can_id)
-            canfunc.set_state(can_id, config.requested_state)
-            canfunc.set_control_mode(can_id, config.control_mode, config.input_mode)
-            canfunc.set_limits(can_id, config.velocity_limit, config.current_limit)
-            canfunc.set_trap_traj_limits(can_id, config.trap_traj_velocity_limit, config.trap_traj_acceleration_limit,
-                                         config.trap_traj_deceleration_limit, config.trap_traj_inertia)
-            canfunc.set_gains(can_id, config.position_gain, config.velocity_gain, config.velocity_integrator_gain)
-            if config.id > self.max_motor_id:
+            self.cantool.send_configuration(config.can_id, config)
+            if config.id > self.max_motor_id: # Max motor id = max number of motors
                 self.max_motor_id = config.id
         self.get_logger().info("Motor configurations updated.")
         return response
 
     def get_info_callback(self):
         infos = ODriveInfoArray()
-        infos.infos = [ODriveInfo()] * (self.max_motor_id+1)
+        infos.infos = [ODriveInfo()] * (self.max_motor_id+1) # Allocate for max number of motors
         for config in self.motor_confs:
-
-            info = ODriveInfo()
-            can_id = config.can_id
-
-            info.fault, info.state = canfunc.get_error_and_state(can_id)
-            info.pos_estimate, info.vel_estimate = canfunc.get_position_and_velocity_estimates(can_id)
-            info.bus_voltage, info.bus_current = canfunc.get_bus_voltage_and_current(can_id)
-            info.fet_temperature, info.motor_temperature = canfunc.get_temperatures(can_id)
-
-            #info.iq_setpoint, info.iq_measured = canfunc.get_qcurrent_setpoint_and_measured(can_id)
-            # info.torque_estimate = 8.27 * info.iq_measured / 330
-            # ^ Slow response? Causing issues
-
-            #info.torque_target, info.torque_estimate = canfunc.get_torque_target_and_estimate(can_id)
-            # ^ Not working for some reason?
-
+            info = self.cantool.get_latest_info(config.can_id)
             if (config.id < len(self.last_cmds)):
+                # Add last commanded position to info for reference
                 last_cmd = self.last_cmds[config.id]
                 info.last_pos_cmd = last_cmd.input_position * config.gear_ratio
                 info.last_vel_cmd = last_cmd.input_velocity * config.gear_ratio
                 info.last_torque_cmd = last_cmd.input_torque / config.gear_ratio
-
             infos.time = self.get_clock().now().to_msg()
             infos.infos[config.id] = info
-            
         self.info_pub.publish(infos)
         
     # Put motors in idle
     def idle(self):
         for config in self.motor_confs:
-            canfunc.set_state(config.id, 1) # Idle
+            self.cantool.idle(config.can_id)
 
     # Emergency stop motors
     def emergency_stop(self):
         for config in self.motor_confs:
-            canfunc.emergency_stop(config.id)
+            self.cantool.emergency_stop(config.can_id)
 
 
 # ROS Entry
@@ -123,7 +91,8 @@ def main(args=None):
         pass
     finally:
         node.idle()
-        canfunc._canbus.shutdown()
+        node.cantool.shutdown()
+        node.cannotify.stop()
         node.get_logger().info("Motor driver node closed.")
         node.destroy_node()
         rclpy.shutdown()
