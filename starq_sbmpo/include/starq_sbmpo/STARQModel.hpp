ae9e3a4b5c25f3679bf1034b665f4ae1b0f84f8f
Jonathan Boylan <jtylerboylan@outlook.com>
2023-08-02 11:56:51 -0400

    STARQ Model

--- starq_sbmpo/include/starq_sbmpo/STARQModel.hpp
+++ starq_sbmpo/include/starq_sbmpo/STARQModel.hpp
@@ -1,7 +1,31 @@
 #ifndef STARQ_SBMPO_MODEL_HPP_
 #define STARQ_SBMPO_MODEL_HPP_
 
+#include <sbmpo/models/UnicycleSteeringModel.hpp>
 
+namespace starq_sbmpo {
+
+using namespace sbmpo;
+
+class STARQModel : public sbmpo_models::UnicycleSteeringModel {
+
+public:
+
+    STARQModel() 
+    : sbmpo_models::UnicycleSteeringModel() {
+        // TODO: Register NVBLOX Map subscription
+    }
+
+    bool is_valid(const State& state) override {
+        // TODO: NVBLOX Map obstacle detection
+    }
+
+private:
+
+
+};
+
+}
 
 
 #endif
