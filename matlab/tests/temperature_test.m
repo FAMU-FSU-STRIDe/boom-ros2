119bff4b8a14556863809106cf1d73f01345367f
jtylerboylan <jtylerboylan@outlook.com>
2023-10-03 13:15:24 -0400

    Temp tests

--- matlab/tests/temperature_test.m
+++ matlab/tests/temperature_test.m
@@ -3,13 +3,14 @@ clear
 close all
 %%
 
-PERIOD_ON = 90; % s
-PERIOD_OFF = 90; % s
-TORQUE = -0.25; % N*m
+PERIOD_ON = 300; % s
+PERIOD_OFF = 0; % s
+TORQUE = -0.50; % N*m
 
 node = ros2node("temperature_test_matlab");
 odrv = ODriveController(node, 1);
 odrv.setControlModes(1);
+odrv.setCurrentLimits(15);
 
 odrv.ready
 odrv.startRecording((PERIOD_ON + PERIOD_OFF) * 50)
@@ -28,7 +29,7 @@ hold on
 grid on
 plot(mdata.time, mdata.fet_temp, 'LineWidth', 3);
 plot(mdata.time, mdata.motor_temp, 'LineWidth', 3);
-plot([90 90], [22 40], '--k', 'LineWidth', 1.5)
+%plot([PERIOD_ON PERIOD_ON], [min(mdata.motor_temp) max(mdata.fet_temp)], '--k', 'LineWidth', 1.5)
 xlabel("Time (s)")
 ylabel("Temperature (C)")
 legend(["FET" "MOTOR"])
