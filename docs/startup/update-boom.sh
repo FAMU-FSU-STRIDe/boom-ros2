6715af2947c74121141449ab9ed9d02fc6072147
jtylerboylan <jtylerboylan@outlook.com>
2024-01-12 13:37:58 -0500

    !

--- docs/startup/update-boom.sh
+++ docs/startup/update-boom.sh
@@ -6,7 +6,6 @@ nmcli device wifi connect Vicon password viconORL
 cd /starq_ws/src
 git pull origin main
 cd /starq_ws
-colcon build
 
 nmcli device disconnect wlan0
 nmcli dev wifi hotspot ifname wlan0 ssid StrideBoom password "strideboom"
