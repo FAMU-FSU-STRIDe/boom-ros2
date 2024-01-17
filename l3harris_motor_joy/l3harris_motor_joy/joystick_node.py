fe7305afbde455992992fcbe8470fa0c44752e28
jtylerboylan <jtylerboylan@outlook.com>
2024-01-17 10:28:44 -0500

    Fix

--- l3harris_motor_joy/l3harris_motor_joy/joystick_node.py
+++ l3harris_motor_joy/l3harris_motor_joy/joystick_node.py
@@ -20,16 +20,23 @@ class JoyStickNode(Node):
 
         self.pi_pwm = GPIO.PWM(12, 10000)
         self.pi_pwm.start(0)
+        self.pi_pwm.ChangeDutyCycle(0.0)
 
         self.get_logger().info("Joystick Node Started")
         self.joy_sub = self.create_subscription(Joy, 'joy', self.joy_callback, 10)
 
         self.odrive_pub = self.create_publisher(ODriveCommandArray, 'odrive', 10)
 
+        self.last_forward_control = 0.0
+        self.last_angle_control = 0.0
+
     def joy_callback(self, msg):
-        self.get_logger().info("Joystick Callback")
         forward_control = msg.axes[1]
         angle_control = msg.axes[0]
+        if (abs(forward_control - self.last_forward_control) < 0.001):
+            return
+        if (abs(angle_control - self.last_angle_control) < 0.001):
+            return
         self.setDirection(forward_control)
         self.setJointAngle(angle_control)
         
