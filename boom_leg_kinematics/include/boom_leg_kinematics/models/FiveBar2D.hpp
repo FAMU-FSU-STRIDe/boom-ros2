62ca7dfd72943dd6e88d26b31a11e74bce594e2c
Jonathan Boylan <jtylerboylan@outlook.com>
2024-05-24 15:38:26 -0400

    kin

--- boom_leg_kinematics/include/boom_leg_kinematics/models/FiveBar2D.hpp
+++ boom_leg_kinematics/include/boom_leg_kinematics/models/FiveBar2D.hpp
@@ -5,68 +5,77 @@
 
 #include <boom_leg_kinematics/KinematicModel.hpp>
 
-namespace boom {
+namespace boom
+{
 
-class FiveBar2DModel : public KinematicModel {
+    class FiveBar2DModel : public KinematicModel
+    {
 
-enum CartesianIndices {X, Y};
-enum MotorAngleIndices {A, B};
+        enum CartesianIndices
+        {
+            X,
+            Y
+        };
+        enum MotorAngleIndices
+        {
+            A,
+            B
+        };
 
-public:
+    public:
+        FiveBar2DModel(const float L1, const float L2, const float GR1, const float GR2)
+            : L1_(L1), L2_(L2), GR1_(GR1), GR2_(GR2) {}
 
-    FiveBar2DModel(const float L1, const float L2, const float GR1, const float GR2)
-    : L1_(L1), L2_(L2), GR1_(GR1), GR2_(GR2) {}
+        ODriveCommandArray get_inverse(const LegCommand &cmd) override
+        {
 
-    ODriveCommandArray get_inverse(const LegCommand& cmd) override {
+            const std::vector<float> point = cmd.input_pos;
 
-        const std::vector<float> point = cmd.input_pos;
+            if (point.size() != 2)
+                return ODriveCommandArray();
 
-        if (point.size() != 2)
-            return ODriveCommandArray();
+            const float theta0 = std::atan2(-point[Y], point[X]);
+            const float theta1 = std::atan2(-point[Y], -point[X]);
 
-        const float theta0 = std::atan2(point[Y], point[X]);
-        const float theta1 = std::atan2(point[Y], -point[X]);
+            const float R = std::sqrt(point[X] * point[X] + point[Y] * point[Y]);
+            const float alpha = std::acos((R * R + L1_ * L1_ - L2_ * L2_) / (2.0f * R * L1_));
 
-        const float R = std::sqrt(point[X]*point[X] + point[Y]*point[Y]);
-        const float alpha = std::acos((R*R + L1_*L1_ - L2_*L2_) / (2.0f*R*L1_));
+            ODriveCommandArray cmds;
+            cmds.commands.resize(2);
+            cmds.commands[A].input_position = GR1_ * (theta0 - alpha) / (2.0 * M_PI);
+            cmds.commands[B].input_position = GR2_ * (theta1 - alpha) / (2.0 * M_PI);
+            return cmds;
+        }
 
-        ODriveCommandArray cmds;
-        cmds.commands.resize(2);
-        cmds.commands[A].input_position = GR1_ * (theta0+alpha)/(2.0*M_PI);
-        cmds.commands[B].input_position = GR2_ * (theta1+alpha)/(2.0*M_PI);
-        return cmds;
-    }
+        LegInfo get_forward(const ODriveInfoArray &angles) override
+        {
+            (void)angles;
 
-    LegInfo get_forward(const ODriveInfoArray& angles) override {
-        (void) angles;
-        
-        std::vector<float> position;
-        for (auto info : angles.infos)
-            position.push_back(info.pos_estimate);
-        
-        if (position.size() != 2)
-            return LegInfo();
+            std::vector<float> position;
+            for (auto info : angles.infos)
+                position.push_back(info.pos_estimate);
 
-        const float alpha = 0.5f*(M_PI - position[A]/GR1_ - position[B]/GR2_);
-        const float gamma = std::asin(L1_*std::sin(alpha)/L2_);
-        const float phi = M_PI - alpha - gamma;
+            if (position.size() != 2)
+                return LegInfo();
 
-        const float theta = position[A] + alpha;
-        const float R = L2_*std::sin(phi)/std::sin(alpha);
+            const float alpha = 0.5f * (M_PI - position[A] / GR1_ - position[B] / GR2_);
+            const float gamma = std::asin(L1_ * std::sin(alpha) / L2_);
+            const float phi = M_PI - alpha - gamma;
 
-        const float X = R*std::cos(theta);
-        const float Y = R*std::sin(theta);
+            const float theta = -(position[A] + alpha);
+            const float R = L2_ * std::sin(phi) / std::sin(alpha);
 
-        LegInfo info;
-        info.pos_estimate = {X, Y};
-        return info;
-    }
+            const float X = R * std::cos(theta);
+            const float Y = R * std::sin(theta);
 
-private:
+            LegInfo info;
+            info.pos_estimate = {X, Y};
+            return info;
+        }
 
-    const float L1_, L2_, GR1_, GR2_;
-
-};
+    private:
+        const float L1_, L2_, GR1_, GR2_;
+    };
 
 }
 
